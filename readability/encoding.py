63cf4376488ac5dbeab0275bee2fad3977d31fbb
more4less <more4less@mailinator.com>
2015-04-09 01:05:12 +0300

    use the latest valid encoding instead of the first

--- readability/encoding.py
+++ readability/encoding.py
@@ -1,24 +1,25 @@
 import re
 import chardet
 
+
+charset_re = re.compile(r'<meta.*?charset=["\']*(.+?)["\'>]', flags=re.I)
+pragma_re = re.compile(r'<meta.*?content=["\']*;?charset=(.+?)["\'>]', flags=re.I)
+xml_re = re.compile(r'^<\?xml.*?encoding=["\']*(.+?)["\'>]')
+
+
 def get_encoding(page):
     # Regex for XML and HTML Meta charset declaration
-    charset_re = re.compile(r'<meta.*?charset=["\']*(.+?)["\'>]', flags=re.I)
-    pragma_re = re.compile(r'<meta.*?content=["\']*;?charset=(.+?)["\'>]', flags=re.I)
-    xml_re = re.compile(r'^<\?xml.*?encoding=["\']*(.+?)["\'>]')
-
     declared_encodings = (charset_re.findall(page) +
             pragma_re.findall(page) +
             xml_re.findall(page))
 
     # Try any declared encodings
-    if len(declared_encodings) > 0:
-        for declared_encoding in declared_encodings:
-            try:
-                page.decode(custom_decode(declared_encoding))
-                return custom_decode(declared_encoding)
-            except UnicodeDecodeError:
-                pass
+    for declared_encoding in reversed(declared_encodings):
+        try:
+            page.decode(custom_decode(declared_encoding))
+            return custom_decode(declared_encoding)
+        except UnicodeDecodeError:
+            pass
 
     # Fallback to chardet if declared encodings fail
     text = re.sub('</?[^>]*>\s*', ' ', page)
--- readability/htmls.py
+++ readability/htmls.py
@@ -1,7 +1,6 @@
 from cleaners import normalize_spaces, clean_attributes
 from encoding import get_encoding
 from lxml.html import tostring
-import logging
 import lxml.html
 import re, sys
 
